e00b5fc9a9ff996afde90c3690459ce323397629
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2020-09-12 10:16:51 +0300

    Add files via upload

---
+++ EBSAdministrator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+//Online İletişim: https://discord.gg/wZskutU
+namespace EBSBaslatici
+{
+    public class EBSAdministrator
+    {
+        public  void AdminRelauncher()
+        {
+            if (!IsRunAsAdmin())
+            {
+                ProcessStartInfo proc = new ProcessStartInfo();
+                proc.UseShellExecute = true;
+                proc.WorkingDirectory = Environment.CurrentDirectory;
+                proc.FileName = Assembly.GetEntryAssembly().CodeBase;
+
+                proc.Verb = "runas";
+
+                try
+                {
+                    Process.Start(proc);
+                    Application.Exit();
+                }
+                catch{}
+            }
+        }
+        private bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
